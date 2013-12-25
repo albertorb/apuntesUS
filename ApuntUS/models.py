5f7e42b33d640a224807646c9b1e8f88e0679672
albrinbor <arincon1992@gmail.com>
2013-12-25 14:18:09 +0100

    modelo de datos completo con 3 clases: asignatura, apuntes, puntuación

--- ApuntUS/models.py
+++ ApuntUS/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 #Declaración de los modelos de la aplicación
 
+class asignatura(models.Model):
+    nombre = models.TextField()
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class apuntes(models.Model):
+    nombre = models.TextField(verbose_name='nombre')
+    descripcion = models.TextField(verbose_name='descripción')
+    enlace = models.URLField(verbose_name='enlace')
+    asig = models.OneToOneField(asignatura, verbose_name='Asignatura')
+
+    def __unicode__(self):
+        return self.nombre + ' (' + self.asig.nombre + ')'
+
+
+class puntuacion(models.Model):
+    apunte = models.OneToOneField(apuntes)
+    puntos = models.IntegerField()
+
+    def __unicode__(self):
+        return self.puntos
